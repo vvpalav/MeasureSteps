dcd3733eefa482bb4866fd114559145ef00ab084
vinayakpalav <vvp221@nyu.edu>
2014-09-03 11:53:31 -0500

    removed

--- MeansureSteps/src/com/example/meansuresteps/helpers/onLocationChangedInterface.java
+++
@@ -1,8 +0,0 @@
-package com.example.meansuresteps.helpers;
-
-public interface onLocationChangedInterface {
-
-	public void updateDistance(double miles);
-
-	public void updateNoOfSteps(int steps);
-}
