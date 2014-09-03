181687eb9e9c7f77cb050f4e31a9e19a55536e57
vinayakpalav <vvp221@nyu.edu>
2014-09-03 10:57:47 -0500

    Fix errors

--- MeansureSteps/gen/com/example/meansuresteps/R.java
+++ MeansureSteps/gen/com/example/meansuresteps/R.java
@@ -12,6 +12,7 @@ public final class R {
     }
     public static final class color {
         public static final int background_color=0x7f040000;
+        public static final int black=0x7f040002;
         public static final int inside=0x7f040001;
     }
     public static final class drawable {
